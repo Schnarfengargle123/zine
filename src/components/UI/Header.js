1f2a556703095e7368a8f675c60790b50220e374
Schnarfengargle123 <schnarfengargle123@gmail.com>
2021-07-06 11:28:45 +0100

    Outsourcing css and creating components

---
+++ src/components/UI/Header.js
@@ -0,0 +1,13 @@
+import './Header.module.css';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className="App-header__title">Zine</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
