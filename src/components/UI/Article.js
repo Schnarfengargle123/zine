a2ee02dd9448fd5280fa167b63150a1ae4e8d18c
Schnarfengargle123 <schnarfengargle123@gmail.com>
2021-07-06 15:54:08 +0100

    Fixed article card like button (still doesn't work).

--- src/components/UI/Article.js
+++ src/components/UI/Article.js
@@ -3,16 +3,19 @@ import styles from "./Article.module.css";
 function Article() {
   return (
     <div className={styles.Article}>
-      <h1 className={styles.Article__title}>Cold Turkey</h1>
-      <p>
-        <strong>Turkeys</strong> protest as chickens <em>exempt</em> from "
-        <u>tax-bill</u>".
-      </p>
-      <p className={styles.Article__date}>06 July 21'</p>
-      {/* <p>
-        This is a rather thought provoking and intellectually stimulating story!
-      </p> */}
-      <button className={styles.Article__button}>Read more!</button>
+      <div className={styles.Article__content}>
+        <h1 className={styles.Article__title}>Cold Turkey</h1>
+        <p>
+          <strong>Turkeys</strong> protest as chickens <em>exempt</em> from "
+          <u>tax-bill</u>".
+        </p>
+        <p className={styles.Article__date}>06 July 21'</p>
+        <button className={styles.Article__button}>Read more!</button>
+      </div>
+
+      <div>
+        <p className={styles.Article__favourite}>&#9825;</p>
+      </div>
     </div>
   );
 }
