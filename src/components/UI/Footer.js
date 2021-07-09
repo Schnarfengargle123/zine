6bc0d0fd7455e5d35fb0a2fa86c380b17af9f676
Schnarfengargle123 <schnarfengargle123@gmail.com>
2021-07-09 23:45:20 +0100

    Additional style changes to header naviagtion and footer content

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -1,5 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faInstagram,
+  faVk,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
+
 import styles from "./Footer.module.css";
-import Link from "react-router-dom";
 
 function Footer() {
   return (
@@ -7,19 +16,39 @@ function Footer() {
       <h4 className={styles.Footer__title}>Zine</h4>
       <div className={styles.Footer__content}>
         <div className={styles.Footer__content__social}>
-          <Link href="https://en-gb.facebook.com/">Facebook</Link>
-          <Link href="https://vk.com/?lang=ru">ВКонтакте</Link>
-          <Link href="https://www.instagram.com">Instagram</Link>
-          <Link href="https://twitter.com/?lang=en-gb">Twitter</Link>
+          <Link href="https://en-gb.facebook.com/">
+            <FontAwesomeIcon
+              className={styles.Social__icon}
+              icon={faFacebookSquare}
+            />
+          </Link>
+          <Link href="https://www.instagram.com">
+            <FontAwesomeIcon
+              className={styles.Social__icon}
+              icon={faInstagram}
+            />
+          </Link>
+          <Link href="https://vk.com/?lang=ru">
+            <FontAwesomeIcon className={styles.Social__icon} icon={faVk} />
+          </Link>
+          <Link href="https://twitter.com/?lang=en-gb">
+            <FontAwesomeIcon
+              className={styles.Social__icon}
+              icon={faTwitterSquare}
+            />
+          </Link>
         </div>
-        <div className={styles.Footer__content__social}>
-          <p>
+        <div className={styles.Footer__content__statement}>
+          <p className={styles.Statement__text}>
             This is very interesting text you are reading! Read more to
             understand just why it is so intersting!
           </p>
         </div>
         <div className={styles.Footer__content__sitemap}>
-          <Link to="/our-story">Our Story!</Link>
+          <Link className={styles.Sitemap__link} to="/our-story">Our Story</Link>
+          <Link className={styles.Sitemap__link} to="/our-story">Contributors</Link>
+          <Link className={styles.Sitemap__link} to="/our-story">Vavancies</Link>
+          <Link className={styles.Sitemap__link} to="/our-story">Our Sites</Link>
         </div>
       </div>
     </div>
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -14,18 +14,46 @@ function Header() {
       <nav>
         <header className={styles.Header}>
           <h1 className={styles.Header__title}>Zine</h1>
+
+          <ul className={styles.Header__navigation}>
+            <li>
+              <NavLink
+                className={styles.Navigation__link}
+                activeClassName={styles.Navigation__link__active}
+                to="/topics"
+              >
+                Latest
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={styles.Navigation__link}
+                activeClassName={styles.Navigation__link__active}
+                to="/topics"
+              >
+                Topics
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={styles.Navigation__link}
+                activeClassName={styles.Navigation__link__active}
+                to="/topics"
+              >
+                Trending
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={styles.Navigation__link}
+                activeClassName={styles.Navigation__link__active}
+                to="/topics"
+              >
+                Recent
+              </NavLink>
+            </li>
+          </ul>
         </header>
-        <ul className={styles.Header__navigation}>
-          <li>
-            <NavLink
-              className={styles.Navigation__link}
-              activeClassName={styles.Navigation__link__active}
-              to="/topics"
-            >
-              Topics
-            </NavLink>
-          </li>
-        </ul>
       </nav>
     </div>
   );
