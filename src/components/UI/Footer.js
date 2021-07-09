c7e881029e5db2ffe83f2d98be361cb5d4bfba60
Schnarfengargle123 <schnarfengargle123@gmail.com>
2021-07-09 15:32:37 +0100

    Made changes

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -1,13 +1,26 @@
 import styles from "./Footer.module.css";
+import Link from "react-router-dom";
 
 function Footer() {
   return (
     <div className={styles.Footer}>
       <h4 className={styles.Footer__title}>Zine</h4>
       <div className={styles.Footer__content}>
-        <div className={styles.Footer__content__social}></div>
-        <div className={styles.Footer__content__social}></div>
-        <div className={styles.Footer__content__sitemap}></div>
+        <div className={styles.Footer__content__social}>
+          <Link href="https://en-gb.facebook.com/">Facebook</Link>
+          <Link href="https://vk.com/?lang=ru">ВКонтакте</Link>
+          <Link href="https://www.instagram.com">Instagram</Link>
+          <Link href="https://twitter.com/?lang=en-gb">Twitter</Link>
+        </div>
+        <div className={styles.Footer__content__social}>
+          <p>
+            This is very interesting text you are reading! Read more to
+            understand just why it is so intersting!
+          </p>
+        </div>
+        <div className={styles.Footer__content__sitemap}>
+          <Link to="/our-story">Our Story!</Link>
+        </div>
       </div>
     </div>
   );
